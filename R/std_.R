e5321d318db2cb5c35881d39eeb88fa3e084343c
thanasisn <11601717+thanasisn@users.noreply.github.com>
2022-05-13 09:07:05 +0300

    Auto commit

--- R/std_.R
+++ R/std_.R
@@ -19,11 +19,13 @@ std_setenv_v1 <- function( ScriptName = "",
                            outdir     = "" ){
     is_filelock <- require(filelock)
 
-    # rm(list = (ls()[ls() != ""]))
     require(funr,     quietly = TRUE)
     require(filelock, quietly = TRUE)
+    ## we always work with UTC
     Sys.setenv(TZ = "UTC")
+    ## keep track of execution time
     tic <<- Sys.time()
+    ## get script file name
     Script.Name <<- tryCatch({
         sys.script() },
         error = function(e) {
@@ -35,12 +37,13 @@ std_setenv_v1 <- function( ScriptName = "",
             }
         }
     )
+    ## paths from script name
     basepath <- paste0(  sub("\\.R$","", Script.Name) )
     if (outdir != "") {
         basedir  <- paste0(normalizePath(outdir),"/")
         basepath <- paste0( basedir, basename(basepath))
     }
-
+    ## output locations
     if(!interactive()) {
         pdf( file = paste0( basepath, ".pdf") )
         sink(file = paste0( basepath, ".out"),  split=TRUE)
@@ -59,8 +62,10 @@ std_setenv_v1 <- function( ScriptName = "",
 #' @export
 #'
 std_scriptend_v1 <- function(){
-    tac <<- Sys.time();
-    cat(sprintf("%s %s@%s %s %f mins\n\n",Sys.time(),Sys.info()["login"],Sys.info()["nodename"],Script.Name,difftime(tac,tic,units="mins")))
+    ## keep track of execution time
+    tac <<- Sys.time()
+    ## print end message
+    cat(sprintf("\n%s %s@%s %s %f mins\n\n",Sys.time(),Sys.info()["login"],Sys.info()["nodename"],Script.Name,difftime(tac,tic,units="mins")))
 }
 
 
